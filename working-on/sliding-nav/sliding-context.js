020567b34817145b7bffbce5122357ad3f93c03b
David Elisma <david.elisma@tbs-sct.gc.ca>
2023-02-06 13:56:40 -0500

    Refactor themes and main html pages
    Update sliding-context.js
    Update sliding-nav-2023.html
    Create contextual.json
    Update themes/*.html
    Update alpha-layered-theme-menu-mr-tp.css

--- working-on/sliding-nav/sliding-context.js
+++ working-on/sliding-nav/sliding-context.js
@@ -1,38 +1,48 @@
-let state = {
-    signin: {
-        link: "",
-        labelExtended: "Sign in"
-    },
-    contextualFooter: { 
-        links: [ 
-            { 
-                url: "someUrl.html", 
-                text: "Contextual link 1"
+( function( $, window, wb  ) {
+    "use strcit";
+
+    var componentName = "gc-mnu",
+        selector = "." + componentName,
+        initEvent = "wb-init" + selector,
+        $document = wb.doc,
+        init = function( event ) {
+            var elm = wb.init( event, componentName, selector ),
+                $elm,
+                settings;
+            if ( elm ) {
+                $elm = $( elm );
+                settings = wb.getData( $elm, componentName );
+                $elm.trigger( "load", settings );
+                wb.ready( $elm, componentName )
             }
-        ]
+        },
+        render = function ( event, data ) {
+            var elm = event.currentTarget,
+                $elm = $( elm );
+
+            if ( elm.getAttribute( "aria-expanded" ) === "true" ) {
+                return
+            } else {
+                $( "button" ).attr( "aria-expanded", false );
+                $elm.attr( "aria-expanded", true );
+            }
+            /* $( "#wb-so a" ).attr( "href" );
+            $( "#wb-so .hidden-xs" ).text( );
+            $each( function( key, value ) {
+                $( "#gc-contextual a" ).text( value.text );
+                $( "#gc-contextual a").attr( "href", value.url );
+            } ) */
+        };
+
+    $document.on( "click", ".gc-mnu button", function( event, data ) {
+        render( event, data )
+    });
+    $document.on( "timerpoke.wb" + initEvent, selector, init );
+    wb.add( selector );
+} )( jQuery, window, wb );
+    
+/*
+
+       
     }
-}
-
-function render() {
-    $( "#wb-so a"  ).attr( "href" );
-    $( "#wb-so .hidden-xs" ).text( state.signin.labelExtended );
-    $each( state.contextualFooter.links, function( key, value ) {
-        $( "#gc-contextual a" ).text( value.text );
-        $( "#gc-contextuùl a ").attr( value.url );
-    } )
-}
-
-$( "[aria-expanded='true']" ).one( "wb-contentupdated", 
-    function( event ) {
-        var gcMnuTheme = event.currentTarget,
-            trigger = gcMnuTheme.getAttribute( "data-trigger-wet" );
-
-        if ( trigger ) {
-            state.signin.link = $( gcMnuTheme ).data( "gcAuth" ).link;
-            state.signin.labelExtended = $( gcMnuTheme ).data( "gcAuth" ).labelExtended;
-            state.contextualFooter.links = $( gcMnuTheme ).data( "gcFooter" ).links
-            render();
-        }
-})
-
-
+*/
